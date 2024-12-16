8d1f921ac206c22de959b1c1f7b4c2b57656e582
P.Yuvadharshan <yuvadharshan2005@gmail.com>
2024-12-16 09:55:34 +0530

    Create stdio.java

---
+++ stdio.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }
+        scanner.close();
+    }
+}
